7e50560c11171a6358969be4aef9ea2f332d1fc4
Zhang Huanjie <james@ustc.edu.cn>
2024-12-04 16:04:58 +0800

    no -t

--- natlog.sh
+++ natlog.sh
@@ -8,6 +8,6 @@ cd /usr/src/gzlog
 
 while true; do
         echo `date` natlog starting >> run.log
-	conntrack -E $@ -b 10240000 | /usr/src/gzlog/natlog_to_flowlog | /usr/src/gzlog/gzlog -p /natlog/natlog -t
+	conntrack -E $@ -b 20480000 | /usr/src/gzlog/natlog_to_flowlog | /usr/src/gzlog/gzlog -p /natlog/natlog
 	sleep 1
 done
