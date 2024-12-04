b034120107cf8741e6efacfc6837f083f8ecbf06
Zhang Huanjie <james@ustc.edu.cn>
2024-12-04 15:27:42 +0800

    buffersize

--- natlog.sh
+++ natlog.sh
@@ -6,6 +6,6 @@ cd /usr/src/gzlog
 
 while true; do
         echo `date` natlog starting >> run.log
-	conntrack -E $@ | /usr/src/gzlog/gzlog -p /natlog/natlog -t
+	conntrack -E $@ -b 10240000 | /usr/src/gzlog/gzlog -p /natlog/natlog -t
 	sleep 1
 done
